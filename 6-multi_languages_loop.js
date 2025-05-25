3530a4ba1bae5d6d4626c571954430ec9d129627
Ahmed Emad <ahmedemad20021907@gmail.com>
2025-05-25 15:47:43 +0300

    Add script printing lines from array using a loop and one console.log

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+let output = "";
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i] + (i === lines.length - 1 ? "" : "\n");
+}
+
+console.log(output);
